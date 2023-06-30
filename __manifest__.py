597636f36325a27214e3c60579564e65958a113c
Rodrigo <kurt.gf@gmail.com>
2023-06-30 12:02:52 -0400

    [FIX]manifest
    Arreglo de dependencias

--- __manifest__.py
+++ __manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','contacts','mail', 'rod_cooperativa'],
+    'depends': ['base','contacts','mail', 'rod_cooperativa','web_notify','web_responsive'],
 
     # always loaded
     'data': [
