7b01e08b4253095053eb1e7963f1f08c3ab7c09e
Rodrigo <kurt.gf@gmail.com>
2024-10-25 15:24:23 -0400

    [FIX]arreglo liquidacion de asociado

--- models/partner_payroll.py
+++ models/partner_payroll.py
@@ -473,7 +473,7 @@ class PartnerPayroll(models.Model):
 
     def finalized_payroll(self):
         total_contributions = (self.capital_initial + self.voluntary_contribution_certificate_total +
-                               self.mandatory_contribution_certificate_total + self.performance_management_total + self.other_contribution_total)
+                               self.mandatory_contribution_certificate_total + self.performance_management_total + self.other_contribution_total + self.surpluses_total)
 
         loan_id = self.env['loan.application'].search([('partner_id','=',self.partner_id.id),('state','=','progress')])
         total_loan_capital_bolivianos = loan_id.balance_capital * loan_id.value_dolar
