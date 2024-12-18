048a22ede0405624ca533c95d4581a798482f04a
Rodrigo <rodrigo.loza.xmarts@gmail.com>
2024-12-18 10:17:24 -0400

    [FIX]arreglos no asociados

--- wizard/wizard_unassociated.py
+++ wizard/wizard_unassociated.py
@@ -21,6 +21,30 @@ class WizardUnassociated(models.TransientModel):
                 record.partner_payroll_id.partner_status_historical = record.partner_payroll_id.partner_id.partner_status
                 record.partner_payroll_id.partner_status_especific_historical = record.partner_payroll_id.partner_id.partner_status_especific
                 record.partner_payroll_id.partner_id.partner_status_especific = 'passive_reserve_a'
+                total_income = 0
+                total_miscellaneous_income = 0
+                total_regulation_cup = 0
+                total_mandatory_contribution_certificate = 0
+                total_voluntary_contribution_certificate = 0
+                total_other_contribution = 0
+                for rec in record.partner_payroll_id.payroll_payments_ids.filtered(
+                        lambda x: x.state != 'no_contribution' and x.state != 'draft'):
+                    total_income += rec.income
+                    total_miscellaneous_income += rec.miscellaneous_income
+                    total_regulation_cup += rec.regulation_cup
+                    total_mandatory_contribution_certificate += rec.mandatory_contribution_certificate
+                    total_voluntary_contribution_certificate += rec.voluntary_contribution_certificate
+                    total_other_contribution += rec.other_contribution
+                create_payroll = record.partner_payroll_id.payroll_payments_ids.create({
+                    'income': -(total_income),
+                    'income_passive': 0,
+                    'miscellaneous_income': -(total_miscellaneous_income),
+                    'regulation_cup': -(total_regulation_cup),
+                    'mandatory_contribution_certificate': -(total_mandatory_contribution_certificate),
+                    'voluntary_contribution_certificate': -(total_voluntary_contribution_certificate),
+                    'other_contribution': -(total_other_contribution),
+                    'state': 'partner_return'
+                })
                 partner_payroll = record.env['partner.payroll'].create({
                     'partner_id': record.partner_payroll_id.partner_id.id,
                     # 'partner_status_especific': 'passive_reserve_a',
@@ -31,6 +55,30 @@ class WizardUnassociated(models.TransientModel):
                 record.partner_payroll_id.partner_status_historical = record.partner_payroll_id.partner_id.partner_status
                 record.partner_payroll_id.partner_status_especific_historical = record.partner_payroll_id.partner_id.partner_status_especific
                 record.partner_payroll_id.partner_id.partner_status_especific = 'passive_reserve_a'
+                total_income = 0
+                total_miscellaneous_income = 0
+                total_regulation_cup = 0
+                total_mandatory_contribution_certificate = 0
+                total_voluntary_contribution_certificate = 0
+                total_other_contribution = 0
+                for rec in record.partner_payroll_id.payroll_payments_ids.filtered(
+                        lambda x: x.state != 'no_contribution' and x.state != 'draft'):
+                    total_income += rec.income
+                    total_miscellaneous_income += rec.miscellaneous_income
+                    total_regulation_cup += rec.regulation_cup
+                    total_mandatory_contribution_certificate += rec.mandatory_contribution_certificate
+                    total_voluntary_contribution_certificate += rec.voluntary_contribution_certificate
+                    total_other_contribution += rec.other_contribution
+                create_payroll = record.partner_payroll_id.payroll_payments_ids.create({
+                    'income': -(total_income),
+                    'income_passive': 0,
+                    'miscellaneous_income': -(total_miscellaneous_income),
+                    'regulation_cup': -(total_regulation_cup),
+                    'mandatory_contribution_certificate': -(total_mandatory_contribution_certificate),
+                    'voluntary_contribution_certificate': -(total_voluntary_contribution_certificate),
+                    'other_contribution': -(total_other_contribution),
+                    'state': 'partner_return'
+                })
                 partner_payroll = record.env['partner.payroll'].create({
                     'partner_id': record.partner_payroll_id.partner_id.id,
                     # 'partner_status_especific': 'passive_reserve_b',
